a027da07e7b020e226ca2ce121649a311e548355
vonseyha <seyhavon22@gmail.com>
2020-08-02 21:40:08 +0700

    Create SubmainPage, DisplayScreen, TabbarFregement, AnswerQuestionPage and SliverToOtherPage

---
+++ lib/Model/CommentModel/commentModelScreen.dart
@@ -0,0 +1,8 @@
+class Trip {
+  final String image;
+  final String username;
+  final DateTime commenntDate;
+  final String description;
+
+  Trip(this.image, this.username, this.commenntDate, this.description);
+}
